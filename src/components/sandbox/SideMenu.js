3057cebb2282f08587494ccdffdcc295a8cfc37a
usersget <2720914944@qq.com>
2023-07-20 10:36:31 +0800

    第二天提交

---
+++ src/components/sandbox/SideMenu.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: usersget
+ * @Date: 2023-07-20 10:31:59
+ * @LastEditors: usersget 2720914944@qq.com
+ * @LastEditTime: 2023-07-20 10:33:45
+ * @FilePath: \newsfeed\src\components\sandbox\Sidemenu.js
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by usersget 2720914944@qq.com All Rights Reserved.
+ */
+import React from 'react'
+
+export default function Sidemenu() {
+  return (
+    <div>
+      Sidemenu
+    </div>
+  )
+}
--- src/components/sandbox/TopHeader.js
+++ src/components/sandbox/TopHeader.js
@@ -1,9 +1,10 @@
+
 import React from 'react'
 
-export default function NewsSandbox() {
+export default function TopHeader() {
   return (
     <div>
-        NewsSandbox
+      TopHeader
     </div>
   )
 }
